c5c2f97771569b7e16a22e09f3321c680421520d
Hazem Fanoush <hazem.fanoush@gmail.com>
2022-04-25 22:49:45 +0200

    Create _animations.scss file and add animations to elements with intersection API

--- js/main.js
+++ js/main.js
@@ -1,30 +1,60 @@
-// Toggle nav button 
-const navBtn = document.querySelector('.nav-btn'),
-    navList = document.querySelector('.nav-list');
+// Toggle nav button
+const navBtn = document.querySelector(".nav-btn"),
+  navList = document.querySelector(".nav-list");
 
 let isToggle = false;
-navBtn.addEventListener('click', () => {
-    if (!isToggle) {
-    navBtn.classList.add('rotate');
-    navList.classList.add('show');
+navBtn.addEventListener("click", () => {
+  if (!isToggle) {
+    navBtn.classList.add("rotate");
+    navList.classList.add("show");
     isToggle = true;
-    } else {
-    navBtn.classList.remove('rotate');
-    navList.classList.remove('show');
+  } else {
+    navBtn.classList.remove("rotate");
+    navList.classList.remove("show");
     isToggle = false;
-    }
+  }
+});
+
+const navBar = document.querySelector(".navbar"),
+  pageHeader = document.querySelector(".page-header");
+
+window.addEventListener("scroll", () => {
+  if (window.scrollY >= 100) {
+    navBar.style.height = "60px";
+    pageHeader.classList.add("active");
+  } else {
+    navBar.style.height = "";
+    pageHeader.classList.remove("active");
+  }
+});
+
+// Collect Elements
+
+function scrollTrigger(selector, options = {}) {
+  const elements = Array.from(document.querySelectorAll(selector));
+
+  elements.forEach((el) => {
+    addObserver(el, options);
+  });
+}
+
+function addObserver(el, options) {
+  let observer = new IntersectionObserver((entries, observer) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add("active");
+
+        observer.unobserve(entry.target);
+      }
+    });
+  }, options);
+
+  observer.observe(el);
+}
+
+scrollTrigger('.landing-wrapper', {
+    rootMargin: '-200px',
 })
-
-
-const navBar = document.querySelector('.navbar'),
-    pageHeader = document.querySelector('.page-header');
-
-window.addEventListener('scroll', () => {
-    if (window.scrollY >= 100) {
-        navBar.style.height = '60px';
-        pageHeader.classList.add('active');
-    } else {
-        navBar.style.height = '';
-        pageHeader.classList.remove('active');
-    }
+scrollTrigger('.section-heading', {
+    rootMargin: '-200px',
 })
